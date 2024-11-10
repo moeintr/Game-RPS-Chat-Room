87dccce1e87f9f7b0cfd5fae8e7970405c6dc7b8
moein <moeintorogh@gmail.com>
2024-11-10 22:06:19 +0330

    9

--- src/com/moein/game/common/DataTypes.java
+++ src/com/moein/game/common/DataTypes.java
@@ -1,5 +1,9 @@
 package com.moein.game.common;
 
+//git checkout feature/merge
+//git merge origin/feature/merge
+//
+//git push origin feature/merge
 public interface DataTypes {
     String NVARCHAR_100 = "NVARCHAR(100)";
     String VARCHAR_20 = "VARCHAR(20)";
